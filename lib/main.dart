e74e3984cb70f6e5b37377b4efbc40445b7a64fc
vrybadplayer <powyieyuaw@gmail.com>
2025-07-07 12:09:08 +0800

    Basic first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 40.0,
+
+            children: [
+              const Text("Login"),
+
+              const TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "Username"
+                ),
+              ),
+
+              const TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                    labelText: "Username"
+                ),
+              ),
+
+              ElevatedButton
+                (onPressed: () {},
+                child: const Text("Login")
+              ),
+
+              ElevatedButton
+                (onPressed: () {},
+                  child: const Text("Register")
+              ),
+
+            ],
+
+          ),
+        ),
+      )
     );
   }
 }
